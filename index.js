5cc803ff0bf15441df5683aad24ee6c520656f43
ejsmith13 <ejsmithwork@gmail.com>
2021-01-26 22:12:43 -0500

    simple questions test

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 const inquirer= require("inquirer")
 const fs= require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {name: "name",
+    message: "what is your name?",
+    type: "input"},
+    {name: "age",
+    message: "What is your Age?",
+    type: "input"}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([...questions])
+    .then((response) =>
+        console.log("sucess")
+    );
+}
 
 // Function call to initialize app
 init();
